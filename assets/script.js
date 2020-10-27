4eefd7ebaa6bbc1dadda9c2cd76324eef8717c3f
calebkw <walke1291@gmail.com>
2020-10-27 08:55:24 -0500

    Added toggle to switch between 12h/24h time formats

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,42 @@ $(document).ready(function()
 {
     let now = moment().format("dddd, MMMM Do YYYY, h:mm a");
     let currentHour = parseInt(moment().format("H"));
+    let timeBlock = $(".container");
+    let startTime = 7;
+    let endTime = 17;
     let hourCheck;
     let minuteInterval;
+    let timeFormat = true;
 
-    updateCurrentTime();
-    updatePastPresent();
-    getSavedEvents();
+    generateTimeBlocks();
+
+    function generateTimeBlocks()
+    {
+        timeBlock.empty();
+
+        for (let i=startTime; i<=endTime; i++)
+        {
+            let row = $("<div>").addClass("row").attr("data-time", i);
+            let timeEl = $("<div>").addClass("col-1 hour")
+            let descriptionEl = $("<textarea>").addClass("col-10 description");
+            let saveBtnEl = $("<button>").addClass("col-1 saveBtn").text("Save");
+
+            if(timeFormat)
+            {
+                timeEl.text(moment({hour:i}).format("h a"));
+            }
+            else
+            {
+                timeEl.text(moment({hour:i}).format("H:mm"));
+            }
+
+            row.append(timeEl, descriptionEl, saveBtnEl);
+            timeBlock.append(row);
+        }
+        updateCurrentTime();
+        setPastPresent();
+        getSavedEvents();
+    }
 
     //Scans for time every second, and updates UI
     function updateCurrentTime()
@@ -19,6 +49,7 @@ $(document).ready(function()
             now = moment().format("dddd, MMMM Do YYYY, h:mm a");
             currentHour = parseInt(moment().format("H"));
             $("#currentDay").text(now);
+            updatePastPresent();
         }, 1000);
 
     }
@@ -28,19 +59,19 @@ $(document).ready(function()
     {
         $("div").each(function()
         { 
-            if (parseInt($(this).attr("value")) === currentHour)
+            if (parseInt($(this).attr("data-time")) === currentHour)
             {
                 $(this).children(".description").addClass("present");
                 $(this).children(".description").removeClass("past");
                 $(this).children(".description").removeClass("future");
             }
-            else if (parseInt($(this).attr("value")) > currentHour)
+            else if (parseInt($(this).attr("data-time")) > currentHour)
             {
                 $(this).children(".description").addClass("future");
                 $(this).children(".description").removeClass("past");
                 $(this).children(".description").removeClass("present");
             }
-            else if (parseInt($(this).attr("value")) < currentHour)
+            else if (parseInt($(this).attr("data-time")) < currentHour)
             {
                 $(this).children(".description").addClass("past");
                 $(this).children(".description").removeClass("present");
@@ -64,7 +95,7 @@ $(document).ready(function()
     {
         $("textarea").each(function()
         {
-            let hour = $(this).parent().attr("value");
+            let hour = $(this).parent().attr("data-time");
 
             if (localStorage.getItem(hour) === null)
             {
@@ -80,10 +111,24 @@ $(document).ready(function()
     //Saves description field text to local storage on button press
     $(".saveBtn").on("click", function()
     {
-        let hour = $(this).parent().attr("value");
+        let hour = $(this).parent().attr("data-time");
 
         let description = $(this).prev().val();
 
         localStorage.setItem(hour, description);
+    })
+
+    $("#time-format").on("click", function()
+    {
+        if ($("#time-format:checked").val() === "on")
+        {
+            timeFormat = true;
+        }
+        else
+        {
+            timeFormat = false;
+        }
+
+        generateTimeBlocks();
     })
 })
