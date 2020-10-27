80cbf010535f74e51da85a5b2261959a67937830
calebkw <walke1291@gmail.com>
2020-10-26 19:31:24 -0500

    Fixed indenting in script.js

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,7 @@ $(document).ready(function()
     setPastPresent();
     getSavedEvents();
 
+    //Scans for time every second, and update UI
     function setCurrentTime()
     {
         $("#currentDay").text(now);
@@ -16,55 +17,55 @@ $(document).ready(function()
             now = moment().format("dddd, MMMM Do YYYY, h:mm a");
         }, 1000);
     }
-
+    
     function setPastPresent()
     {
-    $("div").each(function()
-    { 
-        if (parseInt($(this).attr("value")) === currentHour)
-        {
-        $(this).children(".description").addClass("present");
-        $(this).children(".description").removeClass("past");
-        $(this).children(".description").removeClass("future");
-        }
-        else if (parseInt($(this).attr("value")) > currentHour)
-        {
-        $(this).children(".description").addClass("future");
-        $(this).children(".description").removeClass("past");
-        $(this).children(".description").removeClass("present");
-        }
-        else if (parseInt($(this).attr("value")) < currentHour)
-        {
-        $(this).children(".description").addClass("past");
-        $(this).children(".description").removeClass("present");
-        $(this).children(".description").removeClass("future");
-        }
-    })
+        $("div").each(function()
+        { 
+            if (parseInt($(this).attr("value")) === currentHour)
+            {
+                $(this).children(".description").addClass("present");
+                $(this).children(".description").removeClass("past");
+                $(this).children(".description").removeClass("future");
+            }
+            else if (parseInt($(this).attr("value")) > currentHour)
+            {
+                $(this).children(".description").addClass("future");
+                $(this).children(".description").removeClass("past");
+                $(this).children(".description").removeClass("present");
+            }
+            else if (parseInt($(this).attr("value")) < currentHour)
+            {
+                $(this).children(".description").addClass("past");
+                $(this).children(".description").removeClass("present");
+                $(this).children(".description").removeClass("future");
+            }
+        })
     }
 
     function getSavedEvents()
     {
-    $("textarea").each(function()
-    {
-        let hour = $(this).parent().attr("value");
+        $("textarea").each(function()
+        {
+            let hour = $(this).parent().attr("value");
 
-        if (localStorage.getItem(hour) === null)
-        {
-        return;
-        }
-        else
-        {
-        $(this).val(localStorage.getItem(hour));
-        }
-    })
+            if (localStorage.getItem(hour) === null)
+            {
+                return;
+            }
+            else
+            {
+                $(this).val(localStorage.getItem(hour));
+            }
+        })
     }
 
     $(".saveBtn").on("click", function()
     {
-    let hour = $(this).parent().attr("value");
+        let hour = $(this).parent().attr("value");
 
-    let description = $(this).prev().val();
+        let description = $(this).prev().val();
 
-    localStorage.setItem(hour, description);
+        localStorage.setItem(hour, description);
     })
 })
